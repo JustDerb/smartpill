b04fbf3e8f85de98faa3e09e771a092ff5581fd5
Justin Derby <justderb@gmail.com>
2013-04-02 15:25:01 -0500

    Created basic transport message

--- src/shared/PillMessage.java
+++ src/shared/PillMessage.java
@@ -1,5 +1,24 @@
 package shared;
 
-public class PillMessage {
+import java.io.Serializable;
 
+public class PillMessage implements Serializable {
+
+	/**
+	 * Version for serializing objects
+	 */
+	private static final long serialVersionUID = -6297012409845844272L;
+	
+	public String message;
+	
+	public boolean isSuccess;
+	
+	public PillMessage(String message) {
+		this(message, true);
+	}
+	
+	public PillMessage(String message, boolean success) {
+		this.message = message;
+		this.isSuccess = success;
+	}
 }
