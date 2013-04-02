b3e8530df22627d1b2b97595c3953751125364ea
Jamison Scott Bradley <jsbradley523@gmail.com>
2013-04-02 15:22:24 -0500

    Gui work
    work so far on gui during meeting

--- src/doctor/HomePage.java
+++ src/doctor/HomePage.java
@@ -1,8 +1,6 @@
 package doctor;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -10,13 +8,11 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class HomePage extends JPanel {
 
@@ -25,13 +21,19 @@ public class HomePage extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	//different sections of the home page
+	private JPanel existingPatientSection;
+	private JPanel newPatientSection;
+	private JPanel alertsSection;
 	//Objects used for constructing the GUI
 	private JLabel existingPatientLabel;
 	private Seperator existingSeperator;
 	private EntryField searchField;
+	private JButton searchButton;
 	private JLabel newPatientLabel;
 	private Seperator newPatientSeperator;
-	private EntryField nameField;
+	private EntryField firstNameField;
+	private EntryField lastNameField;
 	private EntryField emailField;
 	private JButton addButton;
 	private JLabel alertsLabel;
@@ -39,125 +41,33 @@ public class HomePage extends JPanel {
 
 	public HomePage(){
 		super(new GridBagLayout());
+		existingPatientSectionInit();
+		newPatientSectionInit();
+		alertsSectionInit();
 		
-		existingPatientLabel = new JLabel("Existing Patient");
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-		gbc.insets = new Insets(3, 3, 0, 0);
+		gbc.weightx = 1;
+		gbc.weighty = 0;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(existingPatientLabel, gbc);
+		add(existingPatientSection, gbc);
 		
-		existingSeperator = new Seperator(3);
 		gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		gbc.weightx = 1;
 		gbc.weighty = 0;
-		gbc.insets = new Insets(3, 0, 3, 0);
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(existingSeperator, gbc);
+		add(newPatientSection, gbc);
 		
-		searchField = new EntryField("Search:");
 		gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = 2;
 		gbc.weightx = 1;
-		gbc.weighty = 0;
-		gbc.insets = new Insets(3, 3, 3, 0);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(searchField, gbc);
-		
-		newPatientLabel = new JLabel("Add New Patient");
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		gbc.weightx = 1;
-		gbc.weighty = 0;
-		gbc.insets = new Insets(20, 3, 0, 0);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(newPatientLabel, gbc);
-		
-		newPatientSeperator = new Seperator(3);
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 4;
-		gbc.weightx = 1;
-		gbc.weighty = 0;
-		gbc.insets = new Insets(3, 0, 3, 0);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(newPatientSeperator, gbc);
-		
-		nameField = new EntryField("Name:");
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 5;
-		gbc.weightx = 1;
-		gbc.weighty = 0;
-		gbc.insets = new Insets(3, 3, 3, 0);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(nameField, gbc);
-		
-		emailField = new EntryField("Email:");
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 6;
-		gbc.weightx = 1;
-		gbc.weighty = 0;
-		gbc.insets = new Insets(3, 3, 3, 0);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(emailField, gbc);
-		
-		addButton = new JButton("Add");
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 7;
-		gbc.weightx = 0;
-		gbc.weighty = 0;
-		gbc.anchor = GridBagConstraints.WEST;
-		gbc.insets = new Insets(3, 3, 3, 0);
-		gbc.fill = GridBagConstraints.NONE;
-		add(addButton, gbc);
-		
-		alertsLabel = new JLabel("Alerts");
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 8;
-		gbc.weightx = 1;
-		gbc.weighty = 0;
-		gbc.insets = new Insets(20, 3, 0, 0);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(alertsLabel, gbc);
-		
-		alertsSeperator = new Seperator(3);
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 9;
-		gbc.weightx = 1;
-		gbc.weighty = 0;
-		gbc.insets = new Insets(3, 0, 3, 0);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(alertsSeperator, gbc);
-		
-		//TODO get rid of spacer and replace with alerts panel
-		JPanel spacer = new JPanel();
-//		spacer.setBorder(BorderFactory.createLineBorder(Color.GREEN));
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 10;
 		gbc.weighty = 1;
 		gbc.fill = GridBagConstraints.BOTH;
-		add(spacer, gbc);
-		
-		
-		addButton.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				//TODO create database entry for the newly added patient
-				System.out.println("adding " + nameField.getText() + " " + emailField.getText());
-			}
-		});
-		
+		add(alertsSection, gbc);
 	}
 	
 	public class Seperator extends JComponent {
@@ -182,6 +92,164 @@ public class HomePage extends JPanel {
 			setSize(width, thickness);
 			g.fillRect(0, 0, width, thickness);
 		}
+	}
+	
+	private void existingPatientSectionInit(){
+		existingPatientSection = new JPanel(new GridBagLayout());
+		
+		existingPatientLabel = new JLabel("Existing Patient");
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.insets = new Insets(3, 3, 0, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		existingPatientSection.add(existingPatientLabel, gbc);
+		
+		existingSeperator = new Seperator(3);
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(3, 0, 3, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		existingPatientSection.add(existingSeperator, gbc);
+		
+		searchField = new EntryField("Search:");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(3, 3, 3, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		existingPatientSection.add(searchField, gbc);
+		
+		searchButton = new JButton("Search");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(3, 3, 3, 0);
+		gbc.fill = GridBagConstraints.NONE;
+		existingPatientSection.add(searchButton, gbc);
+		
+		searchButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println("looking for " + searchField.getText());
+			}
+		});
+	}
+	
+	private void newPatientSectionInit(){
+		newPatientSection = new JPanel(new GridBagLayout());
+		
+		newPatientLabel = new JLabel("Add New Patient");
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(20, 3, 0, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		newPatientSection.add(newPatientLabel, gbc);
+		
+		newPatientSeperator = new Seperator(3);
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(3, 0, 3, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		newPatientSection.add(newPatientSeperator, gbc);
+		
+		firstNameField = new EntryField("First Name:");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(3, 3, 3, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		newPatientSection.add(firstNameField, gbc);
+		
+		lastNameField = new EntryField("Last Name:");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(3, 3, 3, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		newPatientSection.add(lastNameField, gbc);
+		
+		emailField = new EntryField("Email:");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(3, 3, 3, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		newPatientSection.add(emailField, gbc);
+		
+		addButton = new JButton("Add");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(3, 3, 3, 0);
+		gbc.fill = GridBagConstraints.NONE;
+		newPatientSection.add(addButton, gbc);
+		
+		addButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO create database entry for the newly added patient
+				System.out.println("adding " + firstNameField.getText() + " " + lastNameField.getText() + " " + emailField.getText());
+			}
+		});
+	}
+	
+	private void alertsSectionInit(){
+		alertsSection = new JPanel(new GridBagLayout());
+		
+		alertsLabel = new JLabel("Alerts");
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(20, 3, 0, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		alertsSection.add(alertsLabel, gbc);
+		
+		alertsSeperator = new Seperator(3);
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(3, 0, 3, 0);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		alertsSection.add(alertsSeperator, gbc);
+		
+		//TODO get rid of spacer and replace with alerts panel
+		JPanel spacer = new JPanel();
+//		spacer.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.weighty = 1;
+		gbc.fill = GridBagConstraints.BOTH;
+		alertsSection.add(spacer, gbc);
 	}
 	
 	
