1b6c42e67f4b355a427696f38c5378192ee6031e
Justin Derby <justderb@gmail.com>
2013-04-09 15:40:14 -0500

    Added connection stuff into the SQLDatabase

--- src/backend/SQLDatabase.java
+++ src/backend/SQLDatabase.java
@@ -1,25 +1,34 @@
 package backend;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import nogit.SQLCreds;
 
 public class SQLDatabase {
 	/**
-	 * http://stackoverflow.com/questions/5183630/calendar-recurring-repeating-events-best-storage-method
-	 * http://stackoverflow.com/questions/10545869/repeating-calendar-events-and-some-final-maths
+	 * http://stackoverflow.com/questions/5183630/calendar-recurring-repeating-
+	 * events-best-storage-method
+	 * http://stackoverflow.com/questions/10545869/repeating
+	 * -calendar-events-and-some-final-maths
 	 */
-	
+
 	public static String url = SQLCreds.URL;
-    public static String user = SQLCreds.USERNAME;
-    private static String password = SQLCreds.PASSWORD;
-    
-    public static void setLogin(String username, String password) {
-    	SQLDatabase.user = username;
-    	SQLDatabase.password = password;
-    }
-    
-    public static void setURL(String url) {
-    	SQLDatabase.url = url;
-    }
-    
-   
+	public static String user = SQLCreds.USERNAME;
+	private static String password = SQLCreds.PASSWORD;
+
+	public static void setLogin(String username, String password) {
+		SQLDatabase.user = username;
+		SQLDatabase.password = password;
+	}
+
+	public static void setURL(String url) {
+		SQLDatabase.url = url;
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(SQLDatabase.url, SQLDatabase.user,
+				SQLDatabase.password);
+	}
 }
