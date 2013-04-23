54208b1ed49e3c582f7a9cead54273a61f775e13
Justin Derby <justderb@gmail.com>
2013-04-23 14:13:45 -0500

    TCPUIClient has a blocking queue for messages now

--- src/doctor/TCPUIClient.java
+++ src/doctor/TCPUIClient.java
@@ -1,6 +1,7 @@
 package doctor;
 
 import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import shared.DatabaseControl;
 import shared.PillMessage;
@@ -8,25 +9,37 @@ import shared.TCPClient;
 
 public class TCPUIClient extends TCPClient {
 
+	private LinkedBlockingQueue<Object> queue;
+	
 	public TCPUIClient(String IP, int port) throws IOException {
 		super(IP, port);
+	}
+	
+	public Object getResponse()
+	{
+		return queue.poll();
 	}
 
 	@Override
 	public void receivedObject(Object obj, TCPClient client) {
-		if (obj instanceof PillMessage) {
-			// Received general message
-			PillMessage msg = (PillMessage) obj;
-			if (msg.isSuccess) {
-				// Do something...
-			} else {
-				// Show pop up with error (But, do not block!)
-			}
+		try {
+			queue.put(obj);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		else if (obj instanceof DatabaseControl) {
-			// Received a successful CREATE, DELETE, UPDATE call
-			DatabaseControl control = (DatabaseControl)obj;
-		}
+//		if (obj instanceof PillMessage) {
+//			// Received general message
+//			PillMessage msg = (PillMessage) obj;
+//			if (msg.isSuccess) {
+//				// Do something...
+//			} else {
+//				// Show pop up with error (But, do not block!)
+//			}
+//		}
+//		else if (obj instanceof DatabaseControl) {
+//			// Received a successful CREATE, DELETE, UPDATE call
+//			DatabaseControl control = (DatabaseControl)obj;
+//		}
 	}
 
 }
